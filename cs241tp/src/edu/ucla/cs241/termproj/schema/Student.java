b797b8edfa463478989b75265cec6920a826099c
paul.huynh <paul.huynh@2a3b6368-ce57-11de-8bde-2dfea787a966>
2009-11-11 01:19:45 +0000

    Added Student class

---
+++ cs241tp/src/edu/ucla/cs241/termproj/schema/Student.java
@@ -0,0 +1,23 @@
+package edu.ucla.cs241.termproj.schema;
+import java.util.ArrayList;
+
+public class Student extends Person{
+
+	private int sid;
+	private ArrayList<Course> courses;
+	public Student()
+	{
+		
+	}
+	
+	public int getStudentID()
+	{
+		return sid;
+	}
+	
+	public ArrayList<Course> getCourses()
+	{
+		return courses;
+	}
+	
+}
