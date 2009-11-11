0ebe391ffc426e46397b6d739c5889ddb456a1ff
neilconos <neilconos@2a3b6368-ce57-11de-8bde-2dfea787a966>
2009-11-11 00:58:42 +0000

    Testing

--- trunk/cs241tp/src/edu/ucla/cs241/termproj/Main.java
+++ trunk/cs241tp/src/edu/ucla/cs241/termproj/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
+        
+        System.out.println("Hello, World!");
         main.run();
     }
 
