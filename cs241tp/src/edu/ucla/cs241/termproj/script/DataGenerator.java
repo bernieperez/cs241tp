e958656abd5fc200c331886291704a1bf57be87c
bernieperez <bernieperez@2a3b6368-ce57-11de-8bde-2dfea787a966>
2009-11-11 01:01:29 +0000

    Created some organizational packages.

---
+++ cs241tp/src/edu/ucla/cs241/termproj/script/DataGenerator.java
@@ -0,0 +1,12 @@
+package edu.ucla.cs241.termproj.script;
+
+public class DataGenerator {
+
+    /**
+     * Creates data to populate our schema.
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
