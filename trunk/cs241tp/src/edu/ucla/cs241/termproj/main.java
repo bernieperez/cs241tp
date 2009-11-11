aa009990b91a761a17aafa696555a38536171951
bernieperez <bernieperez@2a3b6368-ce57-11de-8bde-2dfea787a966>
2009-11-11 00:32:09 +0000

    Initial Import

---
+++ trunk/cs241tp/src/edu/ucla/cs241/termproj/main.java
@@ -0,0 +1,12 @@
+package edu.ucla.cs241.termproj;
+
+/**
+ * Dummy Class to test check in.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Team Members of CS 241A Term Project!");
+    }
+
+}
