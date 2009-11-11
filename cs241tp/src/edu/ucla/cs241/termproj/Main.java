57436bbb12175e1077ed07263de9f9e958938b25
bernieperez <bernieperez@2a3b6368-ce57-11de-8bde-2dfea787a966>
2009-11-11 00:38:40 +0000

    Okay lets get this party started.

---
+++ cs241tp/src/edu/ucla/cs241/termproj/Main.java
@@ -0,0 +1,17 @@
+package edu.ucla.cs241.termproj;
+
+/**
+ * Dummy Class to test check in.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run() {
+        System.out.println("Hello Team Members of CS 241A Term Project!");        
+    }
+    
+}
