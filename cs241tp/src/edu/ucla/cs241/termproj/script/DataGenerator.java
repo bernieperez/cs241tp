7b3d517708f45f24922e7455b1e50258e38b293a
bernieperez <bernieperez@2a3b6368-ce57-11de-8bde-2dfea787a966>
2009-11-18 01:06:05 +0000

    Added a random name generator and test.

--- cs241tp/src/edu/ucla/cs241/termproj/script/DataGenerator.java
+++ cs241tp/src/edu/ucla/cs241/termproj/script/DataGenerator.java
@@ -1,12 +1,24 @@
 package edu.ucla.cs241.termproj.script;
 
+import de.beimax.janag.Namegenerator;
+
 public class DataGenerator {
 
     /**
      * Creates data to populate our schema.
      */
     public static void main(String[] args) {
-
+        Namegenerator ng = new Namegenerator("languages.txt", "semantics.txt");
+        String[] males = ng.getRandomName("U.S. Human (1990)", "Male - Top 500+", 100);
+        String[] females = ng.getRandomName("U.S. Human (1990)", "Female - Top 500+", 100);
+        for(String name : males) {
+            System.out.println(name);
+        }
+        for(String name : females) {
+            System.out.println(name);
+        }
+        System.out.println();
+        System.out.println("Done Bitch! I just jacked yo code!!!");
     }
 
 }
